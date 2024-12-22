33c43d40fdd772ac2587d33ec583fe2b4865368f
Muzar <muzar5374@gmail.com>
2024-12-22 19:00:33 +0600

    Add files via upload

---
+++ src/main/java/org/example/employeedatabasemanagement/Main.java
@@ -0,0 +1,35 @@
+package org.example.employeedatabasemanagement;
+import java.util.Date;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        EmployeeData employeeData = new EmployeeData();
+
+        employeeData.deleteAllEmployees();
+
+        Employee newEmployee1 = new Employee(0, "John Lee", "Developer", 60000, new Date());
+        employeeData.createEmployee(newEmployee1);
+
+        Employee newEmployee2 = new Employee(0, "Cindy Smith", "Designer", 65000, new Date());
+        employeeData.createEmployee(newEmployee2);
+
+        Employee newEmployee3 = new Employee(0, "Muzar", "Developer", 615000, new Date());
+        employeeData.createEmployee(newEmployee3);
+
+        Employee employee = employeeData.getEmployeeById(1);
+        System.out.println(employee);
+
+        List<Employee> employees = employeeData.getAllEmployees();
+        for (Employee emp : employees) {
+            System.out.println(emp);
+        }
+
+        if (employee != null) {
+            employee.setSalary(65000);
+            employeeData.updateEmployee(employee);
+        }
+
+        employeeData.deleteEmployee(1);
+    }
+}
